dbac67920a44e95b440bbd63e56330eb49dada74
Eric Hartman <imerichartman@gmail.com>
2016-10-19 12:58:37 -0400

    Started ex 6

--- exercise5.rb
+++ exercise5.rb
@@ -1,7 +1,7 @@
-puts "WHAT IS THE TEMPERATURE!?!?! (F)"
-faren = gets.chomp.to_i
-def temp(num)
+def faren(num)
 return (num - 32) * (5.0/9)
 end
+puts "WHAT IS THE TEMPERATURE!?!?! (F)"
+temp = gets.chomp.to_i
 puts "According to the rest of the world"
-puts "The temperature is #{temp(faren)}" 
+puts "The temperature is #{faren(temp)}"
